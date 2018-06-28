c5bac0ba37e82a66f4ce76fbd2076c2781399c8f
吕剑 <2474815284@qq.com>
2018-06-28 21:58:14 +0800

    详情页

--- page/src/page/details/details.js
+++ page/src/page/details/details.js
@@ -4,10 +4,14 @@
 
 require('./details.css');
 var _zz = require('util/zz.js');
+var tab = require('util/switch.js');
 var templateIndex1   = require('./details.string');
 var details ={
 	init : function(){
-
+		var f2 = new tab({
+			tabTarget : $('#f2')
+		});
+		f2.init();
 	}
 };
 
