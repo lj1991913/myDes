ac9a11a100657318b54970a475c0253c74bfd0d5
吕剑 <jian.lv@lebaoedu.com>
2018-07-17 17:39:57 +0800

    xx

---
+++ 2/js/1.js
@@ -0,0 +1,63 @@
+
+
+function Calendar(opts){
+	this.input = opts.input || '';
+	this.priceList = opts.priceList || [];
+	this.m_day = '';
+	this.firstDay = ''; //第一天星期几
+	this.year = '';
+	this.month = '';
+	this.day = '';
+	this.list = '';
+}
+
+Calendar.prototype = {
+	init : function(){
+		var d = new Date(),y;
+		y=d.getFullYear();
+		this.m_day = new Array(31,28+this.is_leap(y),31,30,31,30,31,31,30,31,30,31);
+		this.firstDay = d.getDay();
+		this.day = d.getDate();
+		this.year = y;
+		this.month = d.getMonth()+1;
+		this.list=Math.ceil((this.m_day[this.month-1] + this.firstDay)/7);
+		this.randerHtml();
+
+	},
+	randerHtml : function(){
+		var idx,date_str;
+		for(i=0;i<6;i++) { //表格的行
+		　　for(k=0;k<7;k++) { //表格每行的单元格
+		　　　　idx=i*7+k; //单元格自然序列号
+		　　　　date_str=idx-this.firstDay+1; //计算日期
+				var str ;
+				console.log(date_str);
+				if(date_str<=0){
+					if(this.month>1){
+						date_str = this.m_day[this.month] + date_str;
+					}else{
+						date_str = '';
+					}
+					str = '<div class="calendar-bot-item hui"><span class="days">'+date_str+'</span><span class="rest">休</span><span class="price">￥300</span></div>';
+				}else if(date_str>this.m_day[this.month-1]){
+					if(this.month != 12){
+						date_str =date_str - this.m_day[this.month+1] + 1 ;
+					}else{
+						date_str = '';
+					}
+					str = '<div class="calendar-bot-item hui"><span class="days">'+date_str+'</span><span class="rest">休</span><span class="price">￥300</span></div>';
+				}else{
+
+					date_str=idx-this.firstDay+1;
+					str = '<div class="calendar-bot-item"><span class="days">'+date_str+'</span><span class="rest">休</span><span class="price">￥300</span></div>';
+				}
+				
+				$('#rl').append(str);
+		　　}
+		}
+	},
+	is_leap : function(year){
+		var res;
+		return (year%100==0?res=(year%400==0?1:0):res=(year%4==0?1:0));
+	}
+};
